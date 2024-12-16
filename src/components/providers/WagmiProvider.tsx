93a326250ca77cb726466e571491230f9e8d9714
skuhlmann <samkuhlmann@gmail.com>
2024-12-16 11:59:17 -0700

    chain obj

--- src/components/providers/WagmiProvider.tsx
+++ src/components/providers/WagmiProvider.tsx
@@ -1,7 +1,7 @@
 import { createConfig, http, WagmiProvider } from "wagmi";
-import { sepolia } from "wagmi/chains";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import farcasterFrame from "@farcaster/frame-wagmi-connector";
+import { WAGMI_CHAIN_OBJ } from "~/lib/dao-constants";
 
 // import { injected } from "wagmi/connectors";
 
@@ -9,11 +9,11 @@ import farcasterFrame from "@farcaster/frame-wagmi-connector";
 
 export const config = createConfig({
   // chains: [base, sepolia],
-  chains: [sepolia],
+  chains: [WAGMI_CHAIN_OBJ],
   transports: {
     // Configure dedicated RPC providers when using in production
     // [base.id]: http(),
-    [sepolia.id]: http(),
+    [WAGMI_CHAIN_OBJ.id]: http(),
   },
   connectors: [farcasterFrame()],
   // connectors: [injected()],
